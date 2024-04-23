2d22a82fc5aa8f15e07590f06a70dc4f8d4a3078
2p4n <jdguerrero021422@gmail.com>
2024-04-23 12:30:31 -0500

    Objects

--- ManejoClases/src/manejoclases/Ejecutable.java
+++ ManejoClases/src/manejoclases/Ejecutable.java
@@ -10,11 +10,12 @@ package manejoclases;
  * @author reroes
  */
 public class Ejecutable {
-
     public static void main(String[] args) {
         Hospital hospital = new Hospital();
         Hospital h2 = new Hospital();
         Hospital hospitalLoja = new Hospital();
         Hospital hospital_loja = new Hospital();
+        
+        System.out.println(hospital);
     }
 }
